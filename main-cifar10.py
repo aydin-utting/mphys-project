563116924753070b62dc109dc7334b2e7026ddf5
aydin-utting <72794096+aydin-utting@users.noreply.github.com>
2020-12-03 12:53:02 +0000

    added simple dla

--- main-cifar10.py
+++ main-cifar10.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
+
 #from torchsummary import summary
-from torch.autograd import Variable
+
 import numpy as np
 import PIL
 
@@ -22,7 +23,8 @@ import itertools
 import random
 
 from pruner import Pruner
-from loss_functions import loss_l2,loss_l2_cuda
+from loss_functions import loss_l2, weighted_cross_entropy
+from SimpleDLA import *
 
 # -----------------------------------------------------------
 
@@ -38,7 +40,34 @@ def get_momentum(epoch, p_i, p_f, T):
     
     return p
 
-device = torch.device('cuda')
+
+#Transforms for SimpleDLA
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+
+
+'''
+#transforms from first source for CNN
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -54,21 +83,21 @@ testset = datasets.CIFAR10(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=32,
                                          shuffle=False)
 
-#s=torch.utils.data.RandomSampler(trainset,num_samples = int(len(trainset)*0.1),replacement=True)
-#sub = torch.utils.data.Subset(trainset,s)
-#train_loader = torch.utils.data.DataLoader(s, batch_size=32, shuffle=True)
+s=torch.utils.data.RandomSampler(trainset,num_samples = int(len(trainset)*0.1),replacement=True)
+sub = torch.utils.data.Subset(trainset,s)
+train_loader = torch.utils.data.DataLoader(s, batch_size=32, shuffle=True)
+'''
 
+data_pct = 0.1
+ntrainval = len(trainset)
+TRAIN_PCT = 0.8
+ndata = int(ntrainval*data_pct)
+ntrain = int(ndata*TRAIN_PCT)
+slimmed_data,rest_data =torch.utils.data.random_split(trainset,[ndata,ntrainval-ndata],generator=torch.Generator().manual_seed(42))
+slim_train_data,slim_val_data = torch.utils.data.random_split(slimmed_data,[ntrain,ndata-ntrain],generator=torch.Generator().manual_seed(42))
+slim_train_loader = torch.utils.data.DataLoader(slim_train_data, batch_size=32, shuffle=True)
 
-#data_pct = 0.1
-#ntrainval = len(trainset)
-#TRAIN_PCT = 0.8
-#ndata = int(ntrainval*data_pct)
-#ntrain = int(ndata*TRAIN_PCT)
-#slimmed_data,rest_data =torch.utils.data.random_split(trainset,[ndata,ntrainval-ndata],generator=torch.Generator().manual_seed(42))
-#slim_train_data,slim_val_data = torch.utils.data.random_split(slimmed_data,[ntrain,ndata-ntrain],generator=torch.Generator().manual_seed(42))
-#slim_train_loader = torch.utils.data.DataLoader(slim_train_data, batch_size=32, shuffle=True)
-
-#slim_test_data,rest_test_data =torch.utils.data.random_split(testset,[int(data_pct*len(testset)),len(testset)-int(data_pct*len(testset))],generator=torch.Generator().manual_seed(42))
+slim_test_data,rest_test_data =torch.utils.data.random_split(testset,[int(data_pct*len(testset)),len(testset)-int(data_pct*len(testset))],generator=torch.Generator().manual_seed(42))
 
 # counts = [0 for i in range(10)]
 # for i in range(10):
@@ -76,8 +105,8 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=32,
 
 # print(counts)
 
-classes = ['plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
 params = {'epochs': [200],
@@ -97,8 +126,8 @@ params = {'epochs': [200],
           }
 
 
-#keys, values = zip(*params.items())
-#prune_param_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+keys, values = zip(*params.items())
+prune_param_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
 
 # model= LeNet_KG(in_chan=3, out_chan=10, imsize=32, kernel_size=5)
@@ -126,7 +155,7 @@ param_dict = {'epochs': 200,
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
-epochs        = 50   # number of training epochs
+epochs        = 200   # number of training epochs
 valid_size    = 50    # number of samples for validation
 batch_size    = 50    # number of samples per mini-batch
 imsize        = 50    # image size
@@ -137,23 +166,18 @@ decay         = 1e-6  # weight decay for regularisation
 random_seed   = 42
 p_i = 5e-1
 p_f = 0.99
-T = 100
-gamma = 0.98
+T = 200
+
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
 # model= LeNet_KG(in_chan=3, out_chan=10, imsize=32, kernel_size=5)
-
-outfile = "./mb_lenet_kg.pt"
-
-model = CNN()
-model.load_state_dict(torch.load(outfile))
-
-
+model = SimpleDLA()
 # model, df =  train(model,slim_train_data,slim_val_data,param_dict)
 
 # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=decay)
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=p_i, dampening=p_i)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=p_i, dampening=p_i,weight_decay=5e-4)
+
 # -----------------------------------------------------------------------------
 
 # summary(model, (1, imsize, imsize))
@@ -162,34 +186,14 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=p_i, 
 
 
 
-use_cuda = True
+use_cuda = False
 
 if use_cuda:
     model = model.cuda()
-    #model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
-    torch.backends.cudnn.benchmark = True
-
-std = [[] for i in range(10)]
-model.eval()
-with torch.no_grad():
-    for batch,(x_test,y_test) in enumerate(trainloader):
-        x_test = x_test.cuda()
-        y_test = y_test.cuda()
-        p,s = model(x_test)
-        c = p.argmax(dim=1)
-        for i in range(p.size()[0]):
-            std[c[i]].append( torch.sqrt(F.softplus(s[i,c[i]])).item() )
-
-print(std)
-plt.hist(std,bins=100,histtype='step')
-plt.title('CIFAR-10 aleatoric distribution')
-plt.legend(classes)
-plt.savefig('./cifar10aleadist.png')
-	
+    model = torch.nn.DataParallel(model)
+    cudnn.benchmark = True
 
 
-
-'''
 
 epoch_trainaccs, epoch_testaccs = [], []
 epoch_trainloss, epoch_testloss = [], []
@@ -199,18 +203,17 @@ for epoch in range(epochs):  # loop over the dataset multiple times
     train_losses, train_accs = [], []; acc = 0
     for batch, (x_train, y_train) in enumerate(trainloader):
         print('Batch: ',batch,end='\r')
+        
         if use_cuda:
             x_train = x_train.cuda()
             y_train = y_train.cuda()
         
-        #x_train = Variable(x_train).to(device)
-        #y_train = Variable(y_train).to(device)
         model.zero_grad()
 
         pred, std = model(x_train)
         # add line in here to call sample_sigma()
-        d, yd = model.sample_sigma(pred,std,y_train)
-        loss = F.cross_entropy(pred, y_train) + loss_l2_cuda(d,yd)
+        # d, yd = model.sample_sigma(pred,std,y_train)
+        loss = weighted_cross_entropy(pred,std,y_train)
         loss.backward()
         optimizer.step()
 
@@ -223,11 +226,10 @@ for epoch in range(epochs):  # loop over the dataset multiple times
         model.eval()
         test_losses, test_accs = [], []; acc = 0
         for i, (x_test, y_test) in enumerate(testloader):
-            x_test = x_test.cuda().to(device)
-            y_test = y_test.cuda().to(device)
-            test_pred, test_std = model(x_test)		
-            test_d, test_yd = model.sample_sigma(pred,std,y_train)
-            loss = F.cross_entropy(test_pred, y_test)  + loss_l2_cuda(test_d,test_yd)
+            test_pred, test_std = model(x_test)
+            # test_d, test_yd = model.sample_sigma(pred,std,y_train)
+        
+            test_loss = weighted_cross_entropy(test_pred,test_std,y_test)
             acc = (test_pred.argmax(dim=-1) == y_test).to(torch.float32).mean()
             test_losses.append(loss.item())
             test_accs.append(acc.mean().item())
@@ -249,7 +251,7 @@ print("Final test error: ",100.*(1 - epoch_testaccs[-1]))
 outfile = "./mb_lenet_kg.pt"
 torch.save(model.state_dict(), outfile)
 
-'''
+
 
 
 
